a2415181f6de8db00a2ffedece1430894e5007a5
Rag88 <4555@holbertonschool.com>
2022-05-19 03:16:17 +0000

    Square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A square class definition """
+
+
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size : size of a side in square
+    """
+    def __init__(self, size):
+        self.__size = size
