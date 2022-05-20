34fbc74dfd5d53e4a45f7fc8a613df1ee4cabd92
Rag88 <4555@holbertonschool.com>
2022-05-20 03:12:36 +0000

    Multiply2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     my_dictionary = a_dictionary.copy()
     for i in my_dictionary:
         my_dictionary[i] = my_dictionary[i] * 2
-    return a_dictionary
+    return my_dictionary
