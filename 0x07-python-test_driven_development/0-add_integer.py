4df1c0b36c1f361495eafd63c24d84672362317e
Rag88 <4555@holbertonschool.com>
2022-05-26 05:19:58 +0000

    Add intege4

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -22,8 +22,9 @@ def add_integer(a, b=98):
         The sum of a and b.
     """
 
-    if type(a) not in (float, int):
-        raise TypeError('a must be an integer')
-    if type(b) not in (float, int):
-        raise TypeError('b must be an integer')
-    return int(a) + int(b)
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
