217936cb4a9352522c693f7c365afb55e91f54ad
Rag88 <4555@holbertonschool.com>
2022-06-26 04:56:44 +0000

    Save to file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Module 7-save_to_json_file
-Contains function that writes Python obj to file using JSON represenation
+Module 5-save_to_json_file
+Contains function that writes Python obj to file using JSON representation
 """
 
 
