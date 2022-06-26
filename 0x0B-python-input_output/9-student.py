1f45336b323ab091afe5d87fb566227cb38fe0e8
Rag88 <4555@holbertonschool.com>
2022-06-26 07:49:04 +0000

    Student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module 9-student
+Write a class Student that defines a student
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        class Student that defines a student by:
+        first_name
+        last_name
+        age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation
+        of a Student instance (same as 8-class_to_json.py)
+        """
+        return self.__dict__
