87ec818738b9cebec112d8e9291be26f39d92584
Rag88 <4555@holbertonschool.com>
2022-06-27 00:53:21 +0000

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Write the first class Base
+"""
+
+
+class Base:
+    """
+    defines class Base
+    Class Attributes:
+        __nb_objects
+    Methods:
+        __init__(self, id=None)
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initialize id, increment class attribute if no
+        id and set as id
+	"""
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
