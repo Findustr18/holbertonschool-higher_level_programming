0433273aa41dd00073cf8b7c8028d510780f54f4
Rag88 <4555@holbertonschool.com>
2022-07-08 02:45:20 +0000

    Base Dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,6 +31,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ Returns JSON string representation of list dict """
         if list_dictionaries is None:
