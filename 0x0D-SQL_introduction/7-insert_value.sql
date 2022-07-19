d58548ec3d5e493828cfdfa6ab22abb1bcdc8073
Rag88 <4555@holbertonschool.com>
2022-07-19 05:41:52 +0000

    Insert value

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- insert a new row in the table in this database
 
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Holberton School');
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');
