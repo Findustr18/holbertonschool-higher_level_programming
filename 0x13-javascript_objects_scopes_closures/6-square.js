e8c642c063847fe51f431d555d35f8b89d821b9e
Rag88 <4555@holbertonschool.com>
2022-08-06 22:35:14 +0000

    Square inherit

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
+// class Square that defines a square and inherits from Rectangle of 4-rectangle.js
+
 const BaseSquare = require('./5-square');
 
-module.exports = class Square extends BaseSquare {
+class Square extends BaseSquare {
   charPrint (c) {
     if (c === undefined) {
-      this.print();
-    } else {
-      for (let col = 0; col < this.width; col += 1) {
-        console.log(c.repeat(this.height));
-      }
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
     }
   }
-};
+}
+
+module.exports = Square;
